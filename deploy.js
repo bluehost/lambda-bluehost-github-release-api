4ed2bec4e294ff08a30d59b94fd6f704736527fd
Micah Wood <micah@wpscholar.com>
2020-04-24 17:21:39 -0400

    Update deploy script

--- deploy.js
+++ deploy.js
@@ -8,5 +8,5 @@ if (fs.existsSync(`./${lambda}.zip`)) {
     fs.unlinkSync(`${lambda}.zip`);
 }
 
-execSync(`zip -r ${lambda}.zip .`);
-execSync(`aws lambda update-function-code --function-name ${lambda} --zip-file fileb://${lambda}.zip --profile bluehost`);
+execSync(`zip -r ${lambda}.zip . -x "*.git*" "deploy.js" "local.js" "package*" "readme.md"`);
+execSync(`aws lambda update-function-code --function-name ${lambda} --zip-file fileb://${lambda}.zip --region=us-west-2 --profile bluehost`);
